1bec988ff86274033ebaf84601b5ebbf1ace4adc
Roan Buysse <roan.buysse@telenet.be>
2018-08-19 13:54:32 +0200

    loading pins on map + layot

--- Werkstuk1/PersonalMapTableViewController.swift
+++ Werkstuk1/PersonalMapTableViewController.swift
@@ -1,11 +1,10 @@
-//
+
 //  PersonalMapTableViewController.swift
 //  MyPersonApp
 //
 //  Created by Roan Buysse on 18/08/2018.
 //  Copyright © 2018 Apple Inc. All rights reserved.
 //
-
 import UIKit
 import MapKit
 import CoreLocation
@@ -13,15 +12,19 @@ import CoreLocation
 
 class PersonalMapTableViewController: UITableViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     
-    
+  
     @IBOutlet weak var mapview: MKMapView!
     
     private var locationManager: CLLocationManager!
     private var currentLocation: CLLocation?
+    var people = [Person]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "Kaart"
+
+        loadSampleData()
+        print(people)
         mapview.delegate = self
         
         locationManager = CLLocationManager()
@@ -34,6 +37,14 @@ class PersonalMapTableViewController: UITableViewController, CLLocationManagerDe
             locationManager.requestWhenInUseAuthorization()
             locationManager.startUpdatingLocation()
             mapview.showsUserLocation=true;
+        }
+        
+        for person in people{
+            let location = CLLocationCoordinate2DMake(person.lattitude!, person.longitude!)
+            let dropPin = MKPointAnnotation()
+            dropPin.coordinate = location
+            dropPin.title = "locatie van: " + (person.naam!)
+            mapview.addAnnotation(dropPin)
         }
     }
     
@@ -50,6 +61,19 @@ class PersonalMapTableViewController: UITableViewController, CLLocationManagerDe
             }
         }
     }
+    
+    func loadSampleData() {
+        let person1 = Person(naam: "De Dom Tom", image: UIImage(named: "Sample1"), straat: "kerkstraat", huisnummer: "5",  postcode: "9080", gemeente: "Lochristi", longitude: 3.827135, lattitude: 51.129885, telefoonNummer: "0499665110" )
+        let person2 = Person(naam: "de Ree Ivo", image: UIImage(named: "Sample3"), straat: "kerkstraat", huisnummer: "4",  postcode: "9000", gemeente: "Brussel", longitude: 4.627135, lattitude: 50.129885, telefoonNummer: "0499665110" )
+        let person3 = Person(naam: "Verstraete Kaat", image: UIImage(named: "Sample2"), straat: "kerkstraat", huisnummer: "2",  postcode: "1060", gemeente: "Antwerpe,", longitude: 2.37135, lattitude: 52.429885, telefoonNummer: "04944225110" )
+        
+        
+        
+        people = [person1, person2, person3]
+        tableView.reloadData()
+        
+    }
 }
+
 
 
