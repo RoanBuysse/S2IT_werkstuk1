3a25549484f2652219a77737a7411126c97c6d24
Roan Buysse <roan.buysse@telenet.be>
2018-08-19 09:13:57 +0200

    added tap + updated to swift 4

---
+++ Werkstuk1/PersonalMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PersonalMapViewController.swift
+//  MyPersonApp
+//
+//  Created by Roan Buysse on 18/08/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class PersonalMapViewController: UITapGestureRecognizer {
+
+}
